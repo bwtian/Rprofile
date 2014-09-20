1f2b9a07a3563a263fd47d77f38d301f975fa6bb
Bingwei Tian <bwtian@gmail.com>
2014-09-20 20:58:19 +0900

    / ‘R/setAlias.R’ + ‘R/sourceDir.R’

--- R/setAlias.R
+++ R/setAlias.R
@@ -1,7 +1,9 @@
 
-cd  <- setwd
-pwd <- getwd
-h   <- head
-s   <- summary
-n   <- names
-len <- length
+## WARNING!!! This makes your code less portable/reproducible.
+## Recommended to use in an Interactive CML mode
+  cd  <- setwd
+  pwd <- getwd
+  h   <- head
+  s   <- summary
+  n   <- names
+  len <- length
