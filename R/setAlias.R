d09d3cd7bf592e6d1e1db80f7bfc121ccc7776af
Bingwei Tian <bwtian@gmail.com>
2014-09-21 11:05:30 +0900

    / ‘R/setAlias.R’

--- R/setAlias.R
+++ R/setAlias.R
@@ -1,9 +1,9 @@
 
 ## WARNING!!! This makes your code less portable/reproducible.
 ## Recommended to use in an Interactive CML mode
-  cd  <- setwd()
-  pwd <- getwd()
-  h   <- head()
-  s   <- summary()
-  n   <- names()
-  len <- length()
+  # cd  <- setwd
+  # pwd <- getwd
+  # h   <- head
+  # s   <- summary
+  # n   <- names
+  # len <- length
