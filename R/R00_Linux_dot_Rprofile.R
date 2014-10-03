16dfb9429f6a5021bf1566ee6796152618b36477
t3500 <bwtian@gmail.com>
2014-10-03 17:38:16 +0900

    / ‘R/R00_Linux_dot_Rprofile.R’

--- R/R00_Linux_dot_Rprofile.R
+++ R/R00_Linux_dot_Rprofile.R
@@ -1,9 +1,8 @@
-
 ### This file is sourced by or symbol linked to ~/.Rprofile
 sourceDir <- function(path = ".") {
         for (file in list.files(path, pattern = "\\.[Rr]$")) {
                 source(file.path(path,file))
         }
 }
-sourceDir("~/Dropbox/config/R/rProfile/RprofilesLinux")
-sourceDir("~/Dropbox/config/R/rProfile/RprofilesAuto")
+sourceDir("~/SparkleShare/Rprofile/R/RprofilesLinux")
+sourceDir("~/SparkleShare/Rprofile/R/RprofilesAuto")
