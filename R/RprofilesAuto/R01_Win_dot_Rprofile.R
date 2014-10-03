c99f7f6afe51b394b99bceb4ecc3547f29cbf2b7
Bingwei Tian <bwtian@gmail.com>
2014-10-03 17:47:37 +0900

    - ‘R/RprofilesAuto/R01_Win_dot_Rprofile.R’ < ‘R/R00_Linux_dot_Rprofile.R’ > ‘R/dofFIles/R00_Linux_dot_Rprofile.R’ < ‘R/R01_Win_dot_Rprofile.R’ > ‘R/dofFIles/R01_Win_dot_Rprofile.R’

--- R/RprofilesAuto/R01_Win_dot_Rprofile.R
+++
@@ -1,8 +0,0 @@
-### This file is sourced by or symbol linked to ~/.Rprofile
-sourceDir <- function(path = ".") {
-        for (file in list.files(path, pattern = "\\.[Rr]$")) {
-                source(file.path(path,file))
-        }
-}
-sourceDir("C:/Air/Dropbox/config/R/rProfile/RprofilesAuto")
-## sourceDir("~/Dropbox/config/R/rProfile/RprofilesWin")
