4cdda0bfbe71d32f667736afdf87b91d6f59c49c
t3500 <bwtian@gmail.com>
2014-10-03 17:25:37 +0900

    + ‘R/options.R’

---
+++ R/options.R
@@ -0,0 +1,13 @@
+options(prompt="R:> ", digits=4, show.signif.stars=FALSE)  ## R Prompt
+## WARNING!!! This makes your code less portable/reproducible.
+options(stringsAsFactors=FALSE)
+# Paper size
+options(papersize="a4")
+options(max.print = 20000)
+options(help_type="html")
+Sys.setenv(R_HISTSIZE='100000')
+# General options
+options(digits=15)
+options("width"=80)                # wide display with multiple monitors
+options(tab.width = 8)
+options("digits.secs" = 3)          # show sub-second time stamps
