180494cb0996e738ab0b1599ed81e9d461847aba
lab16 <bwtian@gmail.com>
2014-10-20 17:39:40 +0900

    / ‘R/dofFiles/setRprofileWindows.bat’

--- R/dofFiles/setRprofileWindows.bat
+++ R/dofFiles/setRprofileWindows.bat
@@ -1,3 +1,3 @@
 REM make a environment variable HOME=%USERPROFILE%
 REM make a environment variable R_HOME=C:\Program Files\R\R-3.1.1
-mklink /d "~/R_HOME/etc/Rprofile.site" "~/SparkleShare/Rprofile/R/dotFiles/R01_Win_dot_Rprofile.R"
+mklink "~/R_HOME/etc/Rprofile.site" "~/SparkleShare/Rprofile/R/dotFiles/R01_Win_dot_Rprofile.R"
