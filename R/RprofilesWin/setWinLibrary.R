739eadbff535f45d5bb32c54c4050c8ffb4f25bc
tian <bwtian@gmail.com>
2014-12-12 10:24:46 +0900

    / ‘R/RprofilesWin/setWinLibrary.R’

--- R/RprofilesWin/setWinLibrary.R
+++ R/RprofilesWin/setWinLibrary.R
@@ -1,2 +1,2 @@
-## set a shared Library for all Machine via GitHub
-.libPaths("~/SparkleShare/rLibWin/")
+## set a shared Library for all Windows Machine via GitHub. Be careful for last "/"
+.libPaths("~/SparkleShare/rLibWin")
