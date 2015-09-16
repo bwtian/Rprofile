ca63ad5aac912116ad6ff3066fdccf62f087d423
phd <bwtian@gmail.com>
2015-09-16 15:19:58 +0800

    / ‘R/RprofilesAuto/CRANmirror.R’

--- R/RprofilesAuto/CRANmirror.R
+++ R/RprofilesAuto/CRANmirror.R
@@ -1,5 +1,6 @@
 
 ## Don't ask me for my CRAN mirror every time
+## Japan：http://cran.ism.ac.jp/
 local({r <- getOption("repos")
-       r["CRAN"] <- "http://cran.ism.ac.jp/"
+       r["CRAN"] <- "https://mirrors.ustc.edu.cn/CRAN/"
        options(repos=r)})
