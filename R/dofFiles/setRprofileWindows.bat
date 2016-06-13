8b65d62c9a7dfad06fd7bb4bd9a1f4648cc7bb08
Bingwei Tian <bwtian@gmail.com>
2016-06-13 23:35:02 +0800

    / ‘R/dofFiles/setRprofileWindows.bat’

--- R/dofFiles/setRprofileWindows.bat
+++ R/dofFiles/setRprofileWindows.bat
@@ -2,7 +2,7 @@ echo on
 REM Step1.make a environment variable HOME=%USERPROFILE%
 REM Step2.make a environment variable R_HOME=C:\Program Files\R\R-3.1.1
 REM Step3. mklink "~/.Rprofile" "~/SparkleShare/Rprofile/R/dotFiles/R01_Win_dot_Rprofile.R"
-REM Step4.copy the "~/SparkleShare/Rprofile/R/dotFiles/.Rprofile" to the ~/Rprofile
+REM Step3.copy the "~/SparkleShare/Rprofile/R/dotFiles/.Rprofile" to the ~/Rprofile
 wmic ENVIRONMENT create name="HOME",username="<system>",VariableValue="%USERPROFILE%"
 wmic ENVIRONMENT create name="R_HOME",username="<system>",VariableValue="C:\Program Files\R\R-3.2.5"
 mklink "%USERPROFILE%/.Rprofile" "%USERPROFILE%/SparkleShare/Rprofile/R/dotFiles/R01_Win_dot_Rprofile.R"
