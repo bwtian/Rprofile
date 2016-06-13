4ee5d9414a6fb626e57907068e432bb1f9035de9
Bingwei Tian <bwtian@gmail.com>
2016-06-13 23:36:24 +0800

    < ‘R/dofFiles/鏂板缓鏂囦欢澶?’ > ‘R/dofFiles/back/’ < ‘R/dofFiles/R01_Win_dot_Rprofile.R’ > ‘R/dofFiles/back/R01_Win_dot_Rprofile.R’

--- R/dofFiles/R01_Win_dot_Rprofile.R
+++
@@ -1,8 +0,0 @@
-### This file is sourced by or symbol linked to ~/.Rprofile
-sourceDir <- function(path = ".") {
-        for (file in list.files(path, pattern = "\\.[Rr]$")) {
-                source(file.path(path,file))
-        }
-}
-sourceDir("~/SparkleShare/Rprofile/R/RprofilesWin")
-sourceDir("~/SparkleShare/Rprofile/R/RprofilesAuto")
---
+++ R/dofFiles/back/R01_Win_dot_Rprofile.R
@@ -0,0 +1,8 @@
+### This file is sourced by or symbol linked to ~/.Rprofile
+sourceDir <- function(path = ".") {
+        for (file in list.files(path, pattern = "\\.[Rr]$")) {
+                source(file.path(path,file))
+        }
+}
+sourceDir("~/SparkleShare/Rprofile/R/RprofilesWin")
+sourceDir("~/SparkleShare/Rprofile/R/RprofilesAuto")
