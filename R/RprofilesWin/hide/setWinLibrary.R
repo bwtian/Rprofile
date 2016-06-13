ee6e3cb949ecf60c2751bbecf583c118a8928a6f
Bingwei Tian <bwtian@gmail.com>
2016-06-13 23:40:25 +0800

    + ‘R/RprofilesWin/hide/’ < ‘R/RprofilesWin/hide/setWinLibrary.R’ > ‘R/RprofilesWin/setWinLibrary.R’

--- R/RprofilesWin/hide/setWinLibrary.R
+++
@@ -1,2 +0,0 @@
-## set a shared Library for all Windows Machine via GitHub. Be careful for last "/"
-.libPaths("~/SparkleShare/rLibWin")
---
+++ R/RprofilesWin/setWinLibrary.R
@@ -0,0 +1,2 @@
+## set a shared Library for all Windows Machine via GitHub. Be careful for last "/"
+.libPaths("~/SparkleShare/rLibWin")
